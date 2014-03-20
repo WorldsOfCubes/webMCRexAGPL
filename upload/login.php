a5e24ef04e79b80aead1309de9d87985141fa447
Sergey Zhirov <sergey@worldsofcubes.net>
2014-03-20 07:40:37 +0700

    Returning to standart login-register / Возвращение ко стандартному входу-регистрации

--- upload/login.php
+++ upload/login.php
@@ -5,8 +5,6 @@ require('./system.php');
 
 loadTool('ajax.php');
 loadTool('user.class.php');
-	loadTool('wocapil.class.php');
-	$api = new WoCAPIl();
 
 BDConnect('login');
 
@@ -22,25 +20,17 @@ if (isset($_GET['out'])) {
 	 $tmp_user = new User($name, (strpos($name, '@') === false)? $bd_users['login'] : $bd_users['email']); 
 	 $ajax_message['auth_fail_num'] = (int)$tmp_user->auth_fail_num();
 	 
-	if (!$tmp_user->id()) {
-       
-        if($api->login($name, $pass)){
-BD("INSERT INTO `{$bd_names['users']}` (`{$bd_users['login']}`,`{$bd_users['password']}`,`{$bd_users['ip']}`,`{$bd_users['ctime']}`,`{$bd_users['group']}`) VALUES('".TextBase::SQLSafe($name)."','".MCRAuth::createPass($pass)."','".TextBase::SQLSafe(GetRealIp())."',NOW(),'1')");
-	 $tmp_user = new User($name, (strpos($name, '@') === false)? $bd_users['login'] : $bd_users['email']);
-	 $tmp_user->authenticate($pass);
-        }else{
+	if (!$tmp_user->id()) 
+	
 		aExit(4, lng('AUTH_NOT_EXIST')); 
-        }
-        }
+
 	if ($tmp_user->auth_fail_num() >= 5) CaptchaCheck(6);
-	if($api->login($name, $pass)){
-		BD("INSERT INTO `{$bd_names['users']}` (`{$bd_users['login']}`,`{$bd_users['password']}`,`{$bd_users['ip']}`,`{$bd_users['ctime']}`,`{$bd_users['group']}`) VALUES('".TextBase::SQLSafe($name)."','".MCRAuth::createPass($pass)."','".TextBase::SQLSafe(GetRealIp())."',NOW(),'1')");
-		$tmp_user = new User($name, (strpos($name, '@') === false)? $bd_users['login'] : $bd_users['email']);
-		$tmp_user->authenticate($pass);
-    }else{
+	
+	if (!$tmp_user->authenticate($pass)) { 
+	
 		$ajax_message['auth_fail_num'] = (int)$tmp_user->auth_fail_num();
-		aExit(1, lng('AUTH_FAIL').'.<br /> <a href="#" href="http://worldsofcubes.ru/go/pwd" target="_BLANK" style="color: #656565;">'.lng('AUTH_RESTORE').' ?</a>'); 
-    }
+		aExit(1, lng('AUTH_FAIL').'.<br /> <a href="#" style="color: #656565;" onclick="RestoreStart(); return false;">'.lng('AUTH_RESTORE').' ?</a>'); 
+	}
 	
 	if ($tmp_user->lvl() <= 0) aExit(4, lng('USER_BANNED'));	
 	
--- upload/register.php
+++ upload/register.php
@@ -30,9 +30,7 @@ if (	$config['p_logic'] != 'usual'
 
 BDConnect('register');
 
-loadTool('wocapir.class.php');
 loadTool('user.class.php');
-	$api = new WoCAPIr();
 $rcodes  = array();  
 
 function tryExit() {
@@ -73,7 +71,7 @@ if ($method == 2) {
 
 	exit(View::ShowStaticPage('mail_verification_ok.html', 'other/'));
 }
-aExit(11, "Регистрация временно заблокирована т.к. в ней много дыр.");
+
 RefreshBans();
 	
 $login  = $_POST['login'];
@@ -84,52 +82,57 @@ $female = (!(int)$_POST['female'])? 0 : 1;
 $email  = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL); 
 	
 if (!CanAccess()) aExit(11, lng('IP_BANNED'));
-    tryExit();
 	
 if (empty($login) || empty($pass) || empty($repass) || empty($_POST['email'])) aExit(1, lng('INCOMPLETE_FORM'));
     
 	if (!preg_match("/^[a-zA-Z0-9_-]+$/", $login))  $rcodes[] = 2; 
     if (!preg_match("/^[a-zA-Z0-9_-]+$/", $pass))   $rcodes[] = 3;
     if (!preg_match("/^[a-zA-Z0-9_-]+$/", $repass)) $rcodes[] = 4;
-	if (!$email)                                    $rcodes[] = 12;
+	if (!$email)                                    $rcodes[] = 12;      
 
+    tryExit();
+	
+    $result = BD("SELECT COUNT(*) FROM `{$bd_names['users']}` WHERE `{$bd_users['login']}`='".TextBase::SQLSafe($login)."'");
+	$line   = mysql_fetch_array($result, MYSQL_NUM );
+	
+	if ($line[0]) aExit(5, lng('AUTH_EXIST_LOGIN'));
+	
+    $result = BD("SELECT COUNT(*) FROM `{$bd_names['users']}` WHERE `{$bd_users['email']}`='".TextBase::SQLSafe($email)."'");
+	$line   = mysql_fetch_array($result, MYSQL_NUM );	
+	
+	if ($line[0]) aExit(15, lng('AUTH_EXIST_EMAIL'));
 
 	if ((strlen($login) < 4)  or (strlen($login) > 15))  $rcodes[] = 6;
 	if ((strlen($pass) < 4)   or (strlen($pass) > 15))   $rcodes[] = 7;
 	if ((strlen($repass) < 4) or (strlen($repass) > 15)) $rcodes[] = 8;
-    if (strlen($email) > 50)   							 $rcodes[] = 13;
-	if (strcmp($pass,$repass)) 							 $rcodes[] = 9;
-	$api->register($login, $pass, $repass, $female, $email);
-	//if() {
-		switch ($api->error) {
-			case "bad login": aExit(2, lng('INCORRECT').'. ('.lng('LOGIN').')');
-			case "bad pass": aExit(2, lng('INCORRECT').'. ('.lng('PASS').')');
-			case "bad repass": aExit(2, lng('INCORRECT').'. ('.lng('REPASS').')');
-			case "bad mail": aExit(2, lng('INCORRECT').'. ('.lng('EMAILL').')');
-			
-			case "bad llogin": aExit(2, lng('INCORRECT_LEN').'. ('.lng('LOGIN').')');
-			case "bad lpass": aExit(2, lng('INCORRECT_LEN').'. ('.lng('PASS').')');
-			case "bad lrepass": aExit(2, lng('INCORRECT_LEN').'. ('.lng('REPASS').')');
-			case "bad lmail": aExit(2, lng('INCORRECT_LEN').'. ('.lng('EMAILL').')');
-			case "bad rrepass": aExit(2, lng('REPASSVSPASS').')');
-			
-			case "elogin": aExit(2, lng('AUTH_EXIST_LOGIN'));
-			case "email": aExit(2, lng('AUTH_EXIST_EMAIL'));
-			default : aExit(2, $api->error);
-			case "OK": ;
-		}
-	//}
+    if (strlen($email) > 50)   $rcodes[] = 13;
+	if (strcmp($pass,$repass)) $rcodes[] = 9;			
+
+    tryExit();		
+
+	$verification = ((int)sqlConfigGet('email-verification'))? true : false;
 	
+	if ($verification) $group = 4;
+	else $group = 1;
+	
+	if (!BD("INSERT INTO `{$bd_names['users']}` (`{$bd_users['login']}`,`{$bd_users['password']}`,`{$bd_users['ip']}`,`{$bd_users['female']}`,`{$bd_users['ctime']}`,`{$bd_users['group']}`) VALUES('".TextBase::SQLSafe($login)."','".MCRAuth::createPass($pass)."','".TextBase::SQLSafe(GetRealIp())."',$female,NOW(),'$group')"))
+	  aExit(14);
 
 	$tmp_user = new User(mysql_insert_id());
 	$tmp_user->setDefaultSkin();	
 
     $next_reg = (int) sqlConfigGet('next-reg-time');	
 	 
+	if ($next_reg  > 0) 
+	BD("INSERT INTO `{$bd_names['ip_banning']}` (`IP`,`time_start`,`ban_until`) VALUES ('".TextBase::SQLSafe($_SERVER['REMOTE_ADDR'])."',NOW(),NOW()+INTERVAL $next_reg HOUR)");
 	
+	if (!$verification)
+		aExit(0, lng('REG_COMPLETE') . '. <a href="#" class="btn" onclick="Login();">'.lng('ENTER').'</a>');						   			    
+	else {	
 				
 		if ( $tmp_user->changeEmail($email, true) > 1 ) aExit(14, lng('MAIL_FAIL'));
 	
-	    aExit(0, lng('REG_COMPLETE') .'. '. lng('REG_CONFIRM_INFO'). $api->error);
+	    aExit(0, lng('REG_COMPLETE') .'. '. lng('REG_CONFIRM_INFO'));
+	}
 	unset($tmp_user);
 ?>
